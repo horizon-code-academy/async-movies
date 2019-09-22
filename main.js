72c16ccf51d492bd688b6e1c7420a9e11a8dfa35
unknown <malekbouba@gmail.com>
2019-09-22 15:48:39 +0200

    pagination

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+let movies = [];
+let moviesNb = 0;
+let pagesNb = 0;
+const pageLength = 4;
+let currentPage = 0;
+
 const runderFilm = ({ Title, Year, Runtime, Poster }) => (
     `       
        <div class="col-3 pb-3">
                 <div class="card" >
                     ${Poster ?
-                         `<img src="${Poster}" class="card-img-top" alt="...">` :
-                         `<img src="assets/fallback.jpg" class="card-img-top" alt="...">`}
+        `<img src="${Poster}" class="card-img-top" alt="...">` :
+        `<img src="assets/fallback.jpg" class="card-img-top" alt="...">`}
                     <div class="card-body">
                         <h5 class="card-title">${Title || "Unknown title"}</h5>
                         <p class="card-text">
@@ -20,7 +26,29 @@ const runderFilm = ({ Title, Year, Runtime, Poster }) => (
 const fetchFilmFromAPI = async () => {
     const response = await fetch('https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies');
     const data = await response.json();
-    document.querySelector('.row').innerHTML = data.map(movies => runderFilm(movies)).join('');
+    movies = [...data, ...data, ...data, ...data, ...data, ...data];
+    moviesNb = movies.length;
+    pagesNb = Math.ceil(moviesNb / pageLength);
+    showPage()
 };
+
+function next() {
+    if(currentPage < pagesNb - 1)
+    currentPage++
+    showPage(movies, currentPage)
+    document.querySelector('#pageNb').innerHTML = `Page ${currentPage + 1}`;
+}
+
+function prev() {
+    if(currentPage > 0)
+    currentPage--
+    showPage(movies, currentPage)
+    document.querySelector('#pageNb').innerHTML = `Page ${currentPage + 1}`;
+}
+
+const showPage = () => {
+    const moviesOfPage = movies.slice(currentPage * pageLength, currentPage * pageLength + pageLength)
+    document.querySelector('.row').innerHTML = moviesOfPage.map(movies => runderFilm(movies)).join('');
+}
 
 fetchFilmFromAPI();
