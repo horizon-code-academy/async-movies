c08d26854683b82cc454fad056708b3ecb4e547a
hamdi ben abdelhafidh <hamdi.babdelhafidh@gmail.com>
2019-09-22 13:24:40 +0100

    2 commit projet

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const runderFilm = ({ Title, Year, Runtime, Poster }) => (
     `       
-       <div class="col-4">
-                <div class="card">
+       <div class="col-4 pb-3" style="40px">
+                <div class="card" >
                     <img src="${Poster}" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${Title}</h5><br/>
@@ -13,9 +13,27 @@ const runderFilm = ({ Title, Year, Runtime, Poster }) => (
         </div>
     `
 );
+//const filmes=[...film,...film]
 const fetchFilmFromAPI = async () => {
     const response = await fetch('https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies');
     const data = await response.json();
     document.querySelector('.row').innerHTML = data.map(movies => runderFilm(movies)).join('');
+    /*$('.row').pagination({
+        dataSource: 'https://my-json-server.typicode.com/horizon-code-academy/fake-movies-api/movies=?',
+        locator: 'items',
+        totalNumber: 5,
+        pageSize: 3,
+        ajax: {
+            beforeSend: function() {
+                dataContainer.html('Loading data from flickr.com ...');
+            }
+        },
+        callback: function(data, pagination) {
+            // template method of yourself
+            var html = template(data);
+            dataContainer.html(html);
+        }
+    })*/
 };
+
 fetchFilmFromAPI();
